c6ef00788893c05a43631896cbdef32c82bb202d
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-03 21:26:37 -0500

    add route to get the latest recipes

--- src/routes/ItemRoute.js
+++ src/routes/ItemRoute.js
@@ -6,5 +6,6 @@ const ItemController = require("../controllers/ItemController");
 router.get("/all-recipes", ItemController.getAllRecipes);
 router.get("/recipes", ItemController.getSeachedRecipes);
 router.get("/recipes/:id", ItemController.getSingleRecipe);
+router.get("/latest-recipes", ItemController.getLatestRecipes);
 
 module.exports = router;
