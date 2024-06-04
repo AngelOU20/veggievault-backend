ed02c518aad282d5235ee663998987c0b65cc6a2
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-03 21:26:21 -0500

    add method to get the latest recipes

--- src/controllers/ItemController.js
+++ src/controllers/ItemController.js
@@ -30,4 +30,18 @@ const getSingleRecipe = async (req, res) => {
   }
 };
 
-module.exports = { getAllRecipes, getSeachedRecipes, getSingleRecipe };
+const getLatestRecipes = async (req, res) => {
+  try {
+    const latestRecipes = await Recipe.find().sort({ createdAt: -1 }).limit(4);
+    res.status(200).json(latestRecipes);
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving latest recipes" });
+  }
+};
+
+module.exports = {
+  getAllRecipes,
+  getSeachedRecipes,
+  getSingleRecipe,
+  getLatestRecipes,
+};
